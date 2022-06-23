bc6559664d92ca2af6fc11522c98b511288bbbfe
Samuel Yona Killagane <samyona48@gmail.com>
2022-06-23 21:41:14 +0300

    Part 1 Qn 2

---
+++ part1qn2/WebApplication1/indexOutofRange.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="indexOutofRange.aspx.cs" Inherits="WebApplication1.indexOutofRange" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" align="center">
+        <u><strong><h3>Project Assignment</h3></strong></u>
+        <u><p>Part 1 Qn2</p></u>
+        <div style="height: 86px">
+            <asp:Label ID="Label1" runat="server" Text="Enter number:"></asp:Label>
+            &nbsp;<asp:TextBox ID="enternum" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Label ID="Label2" runat="server" Text="Enter Index:"></asp:Label>
+            &nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="enterIndex" runat="server"></asp:TextBox>
+
+        </div>
+        <div style="height: 57px">
+           
+            <asp:Button ID="Button1" runat="server" Text="Add number" OnClick="Button1_Click" />
+            &nbsp;&nbsp;</div>
+        <asp:Label ID="checklabeled" runat="server"></asp:Label>
+        <br /><br />
+    </form>
+</body>
+</html>
---
+++ part1qn2/WebApplication1/indexOutofRange.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class indexOutofRange : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int[] array = { 12, 65, 89, 7, 8, 55, 74, 1, 2 };
+            int i;
+            int s;
+            bool res1 = int.TryParse(enternum.Text, out i);
+            bool res2 = int.TryParse(enterIndex.Text, out s);
+
+            if (!res1 && !res2)
+            {
+                checklabeled.Text = "Put in a number for both:";
+            }
+            else
+            {
+                try
+                {
+                    array[s - 1] = i;
+                    checklabeled.Text = "Accepted, The Array " + String.Join(",", array);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    checklabeled.Text = "Error: Index out of range: \nTry Again: index from 0 to "+array.Length;
+                }
+            }
+        }
+    }
+}
