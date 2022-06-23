c4c12c96faab15990bb5d47a3c504efe45f3b36c
Samuel Yona Killagane <samyona48@gmail.com>
2022-06-23 21:24:06 +0300

    Intial

---
+++ Web Services/aspApplication.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Services
+{
+    public partial class aspApplication : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Clicksubmit_Click(object sender, EventArgs e)
+        {
+            bool namecheck = string.IsNullOrEmpty(namebox.Text);
+            bool techlistcheck = string.IsNullOrEmpty(techlist.SelectedValue);
+            if (namecheck == false && techlistcheck == false)
+            {
+                empty.Text = "Your name is " + namebox.Text + " & your favourite technology is " + techlist.SelectedItem;
+            }
+            else if (namecheck == false && techlist.SelectedValue == "")
+            {
+                empty.Text = "Your name is " + namebox.Text + " No technology ";
+            }
+            else
+            {
+                empty.Text = "No name enter ";
+            }
+        }
+    }
+}
