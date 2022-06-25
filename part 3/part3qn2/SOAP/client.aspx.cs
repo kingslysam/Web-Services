140f097dae54701b014c11779dfc94be1048d458
Samuel Yona Killagane <samyona48@gmail.com>
2022-06-26 01:07:11 +0300

    Lab

--- part 3/part3qn2/SOAP/client.aspx.cs
+++ part 3/part3qn2/SOAP/client.aspx.cs
@@ -10,6 +10,7 @@ namespace SOAP
 {
     public partial class client : System.Web.UI.Page
     {
+        int randomNumber;
         WebService1SoapClient obj;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,10 +19,38 @@ namespace SOAP
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            
+            int[] lotteryNumbers = new int[6]; 
             int up = Convert.ToInt32(upperBoundBox.Text);
             int low = Convert.ToInt32(lowerBoundBox.Text);
-            int randomNumber = obj.RandomNumberGenerator(low, up);
-            randomNumberLabel.Text = (randomNumber).ToString();
+            for (int i = 0; i < 6; i++)
+            {
+                int randomNumber = obj.RandomNumberGenerator(low, up);
+                bool same = true;
+                while (same)
+                {
+                    if (i == 0)
+                    {
+                        same = false;
+                        lotteryNumbers[i] = randomNumber;
+                    }
+                    else
+                    {
+                        if (randomNumber != lotteryNumbers[i - 1])
+                        {
+                            lotteryNumbers[i] = randomNumber;
+                            same = false;
+                        }
+                        else
+                        {
+                            int random = obj.RandomNumberGenerator(low, up);
+                            randomNumber = random;
+                        }
+                    }
+                }
+                
+            }
+            randomNumberLabel.Text = String.Join(",",lotteryNumbers);
         }
     }
 }
